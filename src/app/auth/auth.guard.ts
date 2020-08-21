50438678e27f52f9d8cbed587e92e2c91991dfcf
user <angelinapovchanych@gmail.com>
2020-08-21 22:40:11 +0300

    auth guard added

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,21 +1,32 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, take } from 'rxjs/operators';
 
-import { AuthService } from './authService';
+import { AuthService } from './auth.service';
 
 @Injectable({providedIn: 'root'})
 export class AuthGuard implements CanActivate {
-  constructor(private authService: AuthService)
+  constructor(
+    private authService: AuthService,
+    private router: Router) {}
 
   canActivate(
     route: ActivatedRouteSnapshot,
     router: RouterStateSnapshot
-  ): boolean | Promise<boolean> | Observable<boolean> {
-    // not subscr to a subj as it is Observ,
-    return this.authService.user.pipe(map( (user) => {
-      return !!user;
-    })
+  ): boolean | Promise<boolean> | Observable<boolean | UrlTree> {
+    // should check if a user is authinticated . Not subscribe to a subj as it is an Observable,
+    return this.authService.user.pipe(
+      // take(1) to avoid ongoing uer subscription
+      take(1),
+      map( (user) => {
+      // converting truish/falsish values to boolean
+      const isAuth = !!user;
+      if(isAuth){
+        return true;
+      } else {
+        return this.router.createUrlTree(['/auth'])
+      }
+    }));
   }
 }
